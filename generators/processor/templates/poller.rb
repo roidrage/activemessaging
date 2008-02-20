28bc94a3acd2c42e1bbb50b84f9fe28c2d96f6d9
anti.god.botherer <anti.god.botherer@14e5c8bd-2825-0410-b006-0d3caba88fa1>
2008-02-20 07:28:15 +0000

    checking in a merbified branch for people to poke at
    git-svn-id: http://activemessaging.googlecode.com/svn/branches/merbified@130 14e5c8bd-2825-0410-b006-0d3caba88fa1

---
+++ generators/processor/templates/poller.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# Make sure stdout and stderr write out without delay for using with daemon like scripts
+STDOUT.sync = true; STDOUT.flush
+STDERR.sync = true; STDERR.flush
+
+#Try to Load Merb
+begin
+  require File.expand_path(File.dirname(__FILE__)+'/../config/boot')
+  require File.expand_path(File.dirname(__FILE__)+'/../config/merb_init')
+rescue LoadError
+  # Load Rails
+  RAILS_ROOT=File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  require File.join(RAILS_ROOT, 'config', 'boot')
+  require File.join(RAILS_ROOT, 'config', 'environment')
+end
+
+# Load ActiveMessaging processors
+ActiveMessaging::load_processors
+
+# Start it up!
+ActiveMessaging::start
